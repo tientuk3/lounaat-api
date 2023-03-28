be93c4b3724021aa34f687d63f8172a398750518
Tuukka <mail@tuukkaorm.io>
2023-03-29 01:12:51 +0300

    added option to blacklist restaurants by name. added implementation to also scrape POR and Tellus restaurants

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
-URL = 'https://www.lounaat.info/00380-helsinki'
+# URL with hardcoded location data
+LOUNAAT_INFO_URL = 'https://www.lounaat.info/valimopolku-9-helsinki'
+POR_URL = 'https://por.fi/menu/'
+POR_HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0'}
+TELLUS_URL = 'https://www.compass-group.fi/menuapi/feed/rss/current-day?costNumber=3105&language=fi'
+
+# list of names of restaurants that you don't want to see. will be partially matched to restaurant name (not case sensitive)
+# example:
+#blacklist = ['lasihelmi', 'valaja']
 blacklist = []
--- parse.py
+++ parse.py
@@ -1,13 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 import re
-from config import URL, blacklist
+from config import LOUNAAT_INFO_URL, POR_URL, POR_HEADERS, TELLUS_URL, blacklist
 
-# scrape the data and return a list of restaurants represented as dict objects
-def get_restaurants():
+def is_blacklisted(restaurant_name):
+    for blacklisted_name in blacklist:
+            if (blacklisted_name.casefold() in restaurant_name.casefold()):
+                return True
+    return False
 
+def get_lounaat_info_restaurants():
     try:
-        response = requests.get(URL)
+        response = requests.get(LOUNAAT_INFO_URL)
     except Exception as e:
         print("fail")
         print(e)
@@ -43,5 +47,60 @@ def get_restaurants():
                 price_tag.extract()
 
             restaurant["dishes"].append(dish.text)
-        restaurants.append(restaurant)
+
+        if name and not is_blacklisted(name):
+            restaurants.append(restaurant)
+
     return restaurants
+
+def get_por_restaurants():
+    try:
+        response = requests.get(POR_URL, headers=POR_HEADERS)
+    except Exception as e:
+        print("fail")
+        print(e)
+        return []
+    
+    soup = BeautifulSoup(response.text, 'html.parser')
+    content = soup.find("div", class_="kt-tab-inner-content-inner") # get the blob of data containing the menu
+    starting_tag = content.find("h2", string=re.compile("^Viikko")) # find the tag before today's menu
+    menu_element = starting_tag.find_next_sibling()
+    menu_element.find("strong").extract() # filter out and discard the date string
+    matched_dishes = re.findall(r"[A-Z].*?(?=\d)", menu_element.text) # list dishes with some regex magic
+
+    restaurants = []
+
+    restaurant = {"name": "POR",
+                "address": "Strömbergintie 1, 00380 Helsinki",
+                "distance": "0m",
+                "dishes": matched_dishes}
+    
+    restaurants.append(restaurant)
+    return restaurants
+
+def get_tellus_restaurants():
+    # missing implementation
+    try:
+        response = requests.get(TELLUS_URL)
+    except Exception as e:
+        print("fail")
+        print(e)
+        return []
+    
+    soup = BeautifulSoup(response.text, features='xml')
+    content = soup.find("description").find_next("description") # get the blob of data containing the menu
+    matched_dishes = re.split(r"<br>\s*", content.text.replace('\n', '')) # list dishes with some regex magic
+
+    restaurants = []
+
+    restaurant = {"name": "Tellus",
+                "address": "Valimopolku 4, 00380 Helsinki",
+                "distance": "0m",
+                "dishes": [x for x in matched_dishes if x != '']}
+    
+    restaurants.append(restaurant)
+    return restaurants
+
+# scrape the data and return a list of restaurants represented as dict objects
+def get_restaurants():
+    return get_por_restaurants() + get_tellus_restaurants() + get_lounaat_info_restaurants()
